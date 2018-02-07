e7a69c7a751350dc06c6dc3aeb84641da3d2a524
harshinimitta <sivaharshinimitta@gmail.com>
2018-02-06 21:52:55 -0500

    removed the hardwiring of properties by value annotation

--- src/main/java/com/vu/repository/HibernateCustomerRepositoryImpl.java
+++ src/main/java/com/vu/repository/HibernateCustomerRepositoryImpl.java
@@ -3,10 +3,13 @@ package com.vu.repository;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Value;
+
 import com.vu.model.Customer;
 
 public class HibernateCustomerRepositoryImpl implements CustomerRepository {
 
+	@Value("${dbUsername}")
 	private String dbUsername;
 	
 	@Override
@@ -22,8 +25,8 @@ public class HibernateCustomerRepositoryImpl implements CustomerRepository {
 		return customers;
 	}
 
-	public void setDbUsername(String dbUsername) {
+	/*public void setDbUsername(String dbUsername) {
 		this.dbUsername = dbUsername;
-	}
+	}*/
 
 }
